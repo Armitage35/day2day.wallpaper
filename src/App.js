d7aa21aa9b8e98fa482f0eeb6a2d0b0536b6ddde
Adrien Dubois Ahlqvist <adrien.dubois35@gmail.com>
2018-04-22 15:01:49 +0000

    activate yarn and gulp

--- src/App.js
+++ src/App.js
@@ -8,9 +8,6 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Day2Day.wallpaper</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
