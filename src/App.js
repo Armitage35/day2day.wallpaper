1ca4d06619fcf0a6c95d1c6b5fa791f176635174
Adrien Dubois Ahlqvist <adrien.dubois35@gmail.com>
2018-04-22 16:07:25 +0000

    init state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  
+  state = {
+    activeView: 'explore',
+    activeCollection: '',
+    activePicture: ''
+  }
+  
   render() {
     return (
       <div className="App">
