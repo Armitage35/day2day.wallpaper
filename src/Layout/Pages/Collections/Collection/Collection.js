0c4940794c9f3124f55074ddbc90b13dcfe051bb
Adrien Dubois Ahlqvist <adrien.dubois35@gmail.com>
2018-04-24 22:15:57 +0000

    build basic collection object

--- src/Layout/Pages/Collections/Collection/Collection.js
+++ src/Layout/Pages/Collections/Collection/Collection.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import Aux from '../../../../hoc/Aux.js';
+import './Collection.css';
+
 
 const Collection = () => {
-    return (
-        <Aux>
-            <p>test</p>
-        </Aux>
-    );
+	
+	const basicObject = {
+		backgroundSize: 'cover',
+		height: '200px'
+	};
+	
+	const collectionCoverStyle = {
+		height: '400px',
+		gridArea: '1 / 1 / 3 / 2',
+		backgroundImage: 'url(https://images.unsplash.com/photo-1512526825361-0918f33a9fca?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6f07dacb0ea13892f056f96625e3bbb2&auto=format&fit=crop&w=1336&q=80)',
+		backgroundSize: 'cover'
+	};
+	
+	const collectionThumbnail1 = {
+		...basicObject,
+		gridArea: '1 / 2 / 2 / 3',
+		backgroundImage: 'url(https://images.unsplash.com/photo-1521904890966-9ef1cc54e241?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=791fbaa6aba59d309e61004e6f590786&auto=format&fit=crop&w=800&q=80)',
+	};
+	
+	const collectionThumbnail2 = {
+		...basicObject,
+		gridArea: '2 / 2 / 3 / 3',
+		backgroundImage: 'url(https://images.unsplash.com/photo-1524272332618-3a94122bb0c1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=dc77b626bf5cd59a31f9cef342d4a6bb&auto=format&fit=crop&w=1500&q=80)',
+	};
+	
+	return (
+		<div className='collection'>
+			<div style={collectionCoverStyle}> </div>
+			<div style={collectionThumbnail1}> </div>
+			<div style={collectionThumbnail2}> </div>
+		</div>
+	);
 };
 
 export default Collection;
