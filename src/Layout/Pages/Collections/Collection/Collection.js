ad0ac28a645566dd8ff4d301578278aa0bcd73ed
Adrien Dubois Ahlqvist <adrien.dubois35@gmail.com>
2018-04-27 22:00:50 +0000

    build a first iteration of individual pictures

--- src/Layout/Pages/Collections/Collection/Collection.js
+++ src/Layout/Pages/Collections/Collection/Collection.js
@@ -8,11 +8,11 @@ const Collection = () => {
 	};
 	
 	const collectionThumbnailTop = {
-		backgroundImage: 'url(https://images.unsplash.com/photo-1521904890966-9ef1cc54e241?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=791fbaa6aba59d309e61004e6f590786&auto=format&fit=crop&w=800&q=80)',
+		backgroundImage: 'url(https://images.unsplash.com/photo-1521904890966-9ef1cc54e241?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=791fbaa6aba59d309e61004e6f590786&auto=format&fit=crop&w=800&q=80)'
 	};
 	
 	const collectionThumbnailBottom = {
-		backgroundImage: 'url(https://images.unsplash.com/photo-1524272332618-3a94122bb0c1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=dc77b626bf5cd59a31f9cef342d4a6bb&auto=format&fit=crop&w=1500&q=80)',
+		backgroundImage: 'url(https://images.unsplash.com/photo-1524272332618-3a94122bb0c1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=dc77b626bf5cd59a31f9cef342d4a6bb&auto=format&fit=crop&w=1500&q=80)'
 	};
 	
 	return (
