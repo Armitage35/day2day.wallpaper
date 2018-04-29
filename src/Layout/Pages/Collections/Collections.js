c163c6c1ffba1c65b9dceaf938d14ade5a6180b7
Adrien Dubois Ahlqvist <adrien.dubois35@gmail.com>
2018-04-29 01:27:48 +0000

    stuck.

--- src/Layout/Pages/Collections/Collections.js
+++ src/Layout/Pages/Collections/Collections.js
@@ -11,33 +11,48 @@ class Collections extends Component {
 	};
 
 	callUnsplashCollection = () => {
+		console.log('start');
+
 		const options = {
 			method: 'GET',
 			url: 'https://api.unsplash.com/collections/featured',
 			qs: { client_id: '87d65f33bedf2944ee1146f5a30ff235a6b37b4faa403b0b877f02f4fbb36a40' }
 		};
 
+		let unsplashResponse;
+
 		request(options, function(error, response, body) {
 			if (error) throw new Error(error);
-			displayCollectionsHandler(JSON.parse(body));
-		}, this.setState({ready: true}));
-		
-		
-		const displayCollectionsHandler = (unsplashResponse) => {
-			let collections = unsplashResponse.map(collection => {
-				return (
-					<Collection 
-					collectionName = {collection.title}
-					cover = {collection.cover_photo.urls.small}
-					topThumbnail = {collection.preview_photos[1].urls.thumb}
-					bottomThumbnail = {collection.preview_photos[2].urls.thumb}
-					key = {collection.title}
-				/>);
-			});
-			console.log(collections);
-		};
+			unsplashResponse = JSON.parse(body);
+			console.log(unsplashResponse);
+			this.unsplashCallback();
+		});
 	}
 
+	unsplashCallback = (unsplashResponse) => {
+		this.setState({ ready: true });
+		// this.displayCollectionsHandler(unsplashResponse);
+		console.log('merde');
+		console.log(unsplashResponse);
+	}
+
+	displayCollectionsHandler = (unsplashResponse) => {
+		let collections = unsplashResponse.map(collection => {
+			return (
+				<Collection 
+				collectionName = {collection.title}
+				cover = {collection.cover_photo.urls.small}
+				topThumbnail = {collection.preview_photos[1].urls.thumb}
+				bottomThumbnail = {collection.preview_photos[2].urls.thumb}
+				key = {collection.title}
+			/>);
+		});
+		console.log(collections);
+		this.ready = collections;
+	};
+
+	ready = <p>100% ready</p>;
+	notReady = <p>not ready</p>;
 
 	componentDidMount() {
 		this.callUnsplashCollection();
@@ -45,12 +60,10 @@ class Collections extends Component {
 
 	render() {
 		if (this.state.ready === true) {
-			return (
-				<p>ready</p>
-			);
+			return this.ready;
 		}
 		else {
-			return <p>not ready</p>
+			return this.notReady;
 		}
 	}
 };
