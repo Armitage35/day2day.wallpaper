4e7d9d56fc8b783a12c8b3443e7c7a2f00fb2fcc
Adrien Dubois Ahlqvist <adrien.dubois35@gmail.com>
2018-05-10 22:57:50 +0000

    add icons to detailed picture

--- src/Layout/Pages/PhotoDetails/PhotoDetails.js
+++ src/Layout/Pages/PhotoDetails/PhotoDetails.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './PhotoDetails.css';
+import heart from './PhotoButtons/icons/heart.js';
+import Download from './PhotoButtons/icons/Download.js';
 
 const PhotoDetails = (props) => {
-	
+
 	const style = {
 		backgroundImage: 'url("https://images.unsplash.com/photo-1508690346831-ff4d6eedd23d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=eded5f9708de6103262c9b51cade5634&auto=format&fit=crop&w=750&q=80")'
 	};
-	
-	const actions = ['Download', 'Like', 'Set as wallpaper'];
-	
+
+	const actions = [Download, heart, 'Set as wallpaper'];
+
 	let buttons = actions.map((button, index) => {
-		return <button className="bttn-unite bttn-sm bttn-primary" key={index}>{button}</button>;
+		return <button className="bttn-unite bttn-md bttn-primary" key={index}>{button}</button>;
 	});
-	
+
 	return (
 		<div className='PhotoDetails' style={style}>
 			<div className='actionButtons'>
