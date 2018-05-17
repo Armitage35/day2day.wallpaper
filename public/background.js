e257cde1c0ef1e68a9da9c5e5c88a3efc53118c2
Adrien Dubois Ahlqvist <adrien.dubois35@gmail.com>
2018-05-17 13:26:34 +0000

    make way for webfontLoader

--- public/background.js
+++ public/background.js
@@ -1,3 +1,4 @@
+// mandatory content for chromeOS apps. Also defines size for the newly created window
 chrome.app.runtime.onLaunched.addListener(function() {
 	chrome.app.window.create('index.html', {
 		'outerBounds': {
