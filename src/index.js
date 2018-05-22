715e6dc3c4c98838fdf705eac925e545af5f4210
Adrien Dubois Ahlqvist <adrien.dubois35@gmail.com>
2018-05-22 13:12:12 +0000

    add a function to load google font from an XHR request

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// loading Quicksand font in a blob format to please chrome apps' security policies
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://fonts.googleapis.com/css?family=Quicksand:400,500", true);
+xhr.responseType = "blob";
+xhr.onreadystatechange = function() {
+	if (xhr.readyState === 4) {
+		var myfontblob = window.URL.createObjectURL(xhr.response);
+		var newStyle = document.createElement('style');
+		newStyle.appendChild(document.createTextNode("@font-face {font-family: Quicksand; font-weight: 400; src: url('" + myfontblob + "') format(woff);}"));
+		document.head.appendChild(newStyle);
+		console.log(myfontblob);
+	}
+};
+xhr.send();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
