c23e3e14c7968ce1aca6eb75de9ecc66ee017183
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2020-02-25 13:14:20 -0500

    Remove unused code in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,7 @@
-// const gulp = require('gulp'),
-// 	sass = require('gulp-sass'),
-	sassPath = './src/**/*.sass';
-
-// gulp.task('styles', () => {
-//     return gulp.src('sass/**/*.scss')
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe(gulp.dest('./css/'));
-// });
-
-// gulp.task('sass', function() {
-// 	return gulp.src(sassPath)
-// 		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-// 		.pipe(gulp.dest('./src'));
-// });
-
-// gulp.task('sass:watch', function() {
-// 	gulp.watch(sassPath, ['sass']);
-// });
-
-// gulp.task('default', ['sass', 'sass:watch']);
-
-
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const del = require('del');
-
-// gulp.task('styles', () => {
-//     return gulp.src('./src/**/*.sass')
-// 	.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-// 	.pipe(gulp.dest('./src'));
-// });
+const sassPath = './src/**/*.sass';
 
 gulp.task('sass', function() {
 	return gulp.src(sassPath)
